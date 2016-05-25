cb4308a2a328697886af3e4fafb1230e9eba6027
Iraj Eshghi <irajeshghi@gmail.com>
2016-05-25 17:05:20 -0400

    plot now also solves linear system

--- plot.py
+++ plot.py
@@ -2,13 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.tri import Triangulation
+import scipy.sparse as spr
 
-def plotting(m):
+def plotting():
 	t = np.loadtxt('t.dat')
 	p = np.loadtxt('p.dat')
 	fu = np.loadtxt('fu.dat')
+	ia = np.loadtxt('ia.dat')
+	ja = np.loadtxt('ja.dat')
+	arr = np.loadtxt('arr.dat')
+
+	M = spr.csr_matrix((arr,(ia,ja)))
+	M = M.toarray()
 	
-	tr = Triangulation(p[:,0],p[:,1],triangles = t[:m,:]-1)
+	x = np.linalg.solve(M,fu)
+	
+	tr = Triangulation(p[:,0],p[:,1],triangles = t-1)
 	ax = Axes3D(plt.gcf())
 	ax.plot_trisurf(tr,x)
 	return 0
