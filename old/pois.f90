ae942c3dc6c7b7e3f715aafe0b0e76bd04d1663e
irajeshghi@gmail.com <irajeshghi@gmail.com>
2016-08-23 16:10:16 -0400

    more restructuring

--- old/pois.f90
+++ old/pois.f90
@@ -1,30 +1,25 @@
 PROGRAM fem
-	USE meshgs
+	USE mesh
 	implicit none
 	integer(kind=8)::nx,ny,N,NT,NV,NB,i,j,k,q !nx=elements in x, ny=elements in y, N=total number of elements
 	integer, dimension(:,:),allocatable::t
 	integer, dimension(:),allocatable::b,row1,col1,row2,col2,ia,ja
-	real(kind=8), dimension(:,:),allocatable::p !array of points, array of triangle vertices, and big L finite element array
+	real, dimension(:,:),allocatable::p !array of points, array of triangle vertices, and big L finite element array
 	real(kind=8), dimension(:),allocatable::fu,val1,val2,x,x_exact,arr !array of vertices along edge, array of <integral>g_i*f
-	real(kind=8), dimension(3,3)::A !We will use this array in the process of finding equations of planes
-	real(kind=8)::det,temp,centx,centy !determinants of matrices, values to insert in sparse matrix
+	real, dimension(3,3)::A !We will use this array in the process of finding equations of planes
+	real::det,temp !determinants of matrices, values to insert in sparse matrix
 	
-!	write(*,*) 'Nx = ' !How many elements along x?
-!	read(*,*) nx
-!!	write(*,*) 'Ny = ' !How many elements along y?
-!	read(*,*) ny
-!	N = nx*ny
-!	allocate(fu(N))	
-!	call buildmesh(nx,ny,p,t,b) !Builds p,t, and b arrays for later use. 
-
-	call distmesh(p,t,b)
+	write(*,*) 'Nx = ' !How many elements along x?
+	read(*,*) nx
+	write(*,*) 'Ny = ' !How many elements along y?
+	read(*,*) ny
+	N = nx*ny !Total number of elements will be obviously nx*ny
+	allocate(fu(N))
 	
+	call buildmesh(nx,ny,p,t,b) !Builds p,t, and b arrays for later use. 
 	NT = size(t(:,1))
 	NV = size(p(:,1))
-	NB = size(b)
-
-	N = NV !Total number of elements will be the number of vertices
-	allocate(fu(N))
+	NB = size(b) 
 	allocate(val1(NT*9),col1(NT*9),row1(NT*9),val2(NT*9),col2(NT*9),row2(NT*9),x(N),x_exact(N))! Allocate sizes of arrays
 	q = 1
 	do i = 1,NT !Loop to build the row and col vectors
@@ -37,34 +32,32 @@ PROGRAM fem
 
 	do i = 1,N
 		fu(i)=0
-	end do
+	end do		
+	
 	q = 1
-	do i = 1,NT !loop through triangles to add entries into the vals vector, thus building the left hand side matrix
+	do i = 1,NT !loop through triangles to add entries into the vals vector, and build the f vector
 		do j =1,3 !We build the array with x1,y1,x2,y2,...
 			A(j,1) = (p(t(i,j),1))
 			A(j,2) = (p(t(i,j),2))
 			A(j,3) = 1.0
 		end do
-			
+		
 		det = abs(threedet(A)) !This computes the determinant of that matrix, which is used to compute the area of the triangle in question
 		call threeinv(A)
-		centx = (p(t(i,1),1)+p(t(i,2),1)+p(t(i,3),1))/(3.0) !x-coord of centroid of triangle
-		centy = (p(t(i,1),2)+p(t(i,2),2)+p(t(i,3),2))/(3.0) !y-coord of centroid of triangle
+
+		
 		do j=0,8
-			val1(q+j) = (-1)*(A(1,j/3+1)*A(1,modulo(j,3)+1)+A(2,j/3+1)*A(2,modulo(j,3)+1))*det*0.5/(centx) !evaluating the integral of the basis functions, multiplied by 1/x (grad-schafranov)
+			val1(q+j) = (-1)*(A(1,j/3+1)*A(1,modulo(j,3)+1)+A(2,j/3+1)*A(2,modulo(j,3)+1))*det*0.5
 		end do
 		q = q+9
-!	end do
-
-!	do i = 1,NT !build right hand side, looping over all triangles
-		centx = (p(t(i,1),1)+p(t(i,2),1)+p(t(i,3),1))/(3.0) !x-coord of centroid of triangle
-		centy = (p(t(i,1),2)+p(t(i,2),2)+p(t(i,3),2))/(3.0) !y-coord of centroid of triangle
-		temp = foo(centx,centy)/(centx) !2d midpoint rule, multiplied by 1/x (grad-schafranov)
+		
+		temp = foo((p(t(i,1),1)+p(t(i,2),1)+p(t(i,3),1))/(3.0),(p(t(i,1),2)+p(t(i,2),2)+p(t(i,3),2))/(3.0)) !2d midpoint rule
 		
 		fu(t(i,1)) = fu(t(i,1)) + det*0.5*temp/3.0 !Here, we add the result of the convolution of the basis function with the right hand side of the Poisson equation, which gives us the right hand side vector in the finite element equation.
 		fu(t(i,2)) = fu(t(i,2)) + det*0.5*temp/3.0
 		fu(t(i,3)) = fu(t(i,3)) + det*0.5*temp/3.0
 	end do
+
 	do i=1,NB !This loops through the b array and sets the corresponding row of L to all zeros except for the L(b(i),b(i)) spot. It also sets the f(b(i) cell to zero. This allows for correct evaluation of the edges.
 		fu(b(i)) = 0
 		do j = 1,NT*9
@@ -96,7 +89,7 @@ PROGRAM fem
 		end if
 	end do
 	
-	k=0
+	k=0	
 	do i = 1,size(col2)  !find size of nonzero parts of the row and column arrays
 		if(col2(i) /= 0) then
 				k=k+1
@@ -118,18 +111,18 @@ PROGRAM fem
 		x(i) = 0
 	end do
 	temp = 1e-6
-	i = NB/4
-	q = i
-	
+	i = nx/2
+	q = nx/2
 	call mgmres_st(N,size(ia),ia,ja,arr,x,fu,i,q,temp,temp)
 	
 	do i = 1,N
 		x_exact(i) = exact(p(i,1),p(i,2))
 	end do
-	
+
 	open(1,file='./files/conv.dat',access='append')
-		write(1,*) maxval(abs(x-x_exact)),sqrt(float(NT))
+		write(1,*) maxval(abs(x-x_exact))
 	close(1)
+
 	
 	open(1,file='./files/p.dat') !write stuff to file before memory release, so that it can be plotted in python
 		  do i = 1,NV
