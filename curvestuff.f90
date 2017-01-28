edd7f7956a13a02ee0e02007b6a6105f0e4b276a
irajeshghi@gmail.com <irajeshghi@gmail.com>
2017-01-28 02:02:16 -0500

    Almost... There...

--- curvestuff.f90
+++ curvestuff.f90
@@ -3,8 +3,8 @@ contains
 
 function upper(theta,tarc) !THIS FUNCTION NEEDS TO BE CHECKED! NOT SURE IF IT'S DOING ITS WORK
 	implicit none
-	real(kind=8),dimension(:)::tarc
-	real(kind=8)::theta
+	real *8,dimension(:)::tarc
+	real *8::theta
 	integer::N,upper,i
 	N = size(tarc)
 	upper = 0
@@ -19,8 +19,8 @@ function upper(theta,tarc) !THIS FUNCTION NEEDS TO BE CHECKED! NOT SURE IF IT'S 
 end function upper
 function lower(theta,tarc) !THIS FUNCTION NEEDS TO BE CHECKED! NOT SURE IF IT'S DOING ITS WORK
 	implicit none
-	real(kind=8),dimension(:)::tarc
-	real(kind=8)::theta
+	real *8,dimension(:)::tarc
+	real *8::theta
 	integer::N,lower,i
 	N = size(tarc)
 	lower = 0
@@ -38,34 +38,34 @@ subroutine derpois(d1,d2,d3,infi,findif,solx,soly,sol,p,t,b,ubx,uby) !Solves poi
 	!ALSO IMPORTANT (less so) once done debugging, outputting ubx,uby, and b is unnecessary
 	use mesh
 	implicit none
-        real(kind=8),dimension(:,:),allocatable::Gn,p
-        real(kind=8),dimension(:),allocatable::tarc,uh,xin,yin,dx,dy,ddx,ddy,rarc,upx,upy,uhn,un,upn,ux,uy,ubx,uby,sol,solx,soly
+        real *8,dimension(:,:),allocatable::Gn,p
+        real *8,dimension(:),allocatable::tarc,uh,xin,yin,dx,dy,ddx,ddy,rarc,upx,upy,uhn,un,upn,ux,uy,ubx,uby,sol,solx,soly
 	!for debugging
-	real(kind=8),dimension(:),allocatable::fux,fuy,fun,fut
+	real *8,dimension(:),allocatable::fux,fuy,fun,fut
 	!\for debugging
-        real(kind=8)::d1,d2,d3,pi,ds,eps,del,kap,L,infi,findif
-        real(kind=8),dimension(2)::that,nhat,der,dder
-	real(kind=8),dimension(2,2)::flipmat
-        real(kind=8),dimension(7)::args
-	real(kind=8)::det,temp
+        real *8::d1,d2,d3,pi,ds,eps,del,kap,L,infi,findif
+        real *8,dimension(2)::that,nhat,der,dder
+	real *8,dimension(2,2)::flipmat
+        real *8,dimension(7)::args
+	real *8::det,temp
 	integer,dimension(:),allocatable::b
 	integer,dimension(:,:),allocatable::t
         integer::i,j,k,N,m,bsize
-	complex(kind=8),dimension(:),allocatable::cxarr
+	complex *16,dimension(:),allocatable::cxarr
 
 	call distmesh(p,t,b,eps,del,kap) !we import the arrays describing the finite element decomposition of the tokamak
 	call switchpars(eps,del,kap,d1,d2,d3)
 
 	bsize = size(b)
 	N = 5*size(b) !we want the size of our edge decomposition to be comparable to that of the FEM, but maybe more accurate
-        pi = 4*atan(1.0)
+        pi = 4*atan(1.0d0)
 
         allocate(xin(N),yin(N),dx(N),dy(N),ddx(N),ddy(N),Gn(N,N))
 	!debugging
 	allocate(fux(N),fuy(N),fun(N),fut(N))
 	!\debugging
 	allocate(rarc(N),uh(N),cxarr(N),uhn(N),un(N),upn(N),ux(N),uy(N),ubx(bsize),uby(bsize))
-        args = (/d1,d2,d3,real(0.7,kind=8),real(infi,kind=8),real(1.0,kind=8),real(0.0,kind=8)/)
+        args = (/d1,d2,d3,0.7d0,infi,1.0d0,0.0d0/)
 	
         call arcparam(real(0.0,kind=8),2*pi,tarc,ds,N,L,d1,d2,d3,infi,findif) !generate a parametrisation of the boundary. Tarc is the array
 ! of angles which give equidistant points along the boundary
@@ -87,12 +87,12 @@ subroutine derpois(d1,d2,d3,infi,findif,solx,soly,sol,p,t,b,ubx,uby) !Solves poi
 	call solveyouh(Gn,xin,yin,dx,dy,upx,upy,uh,N,ds) ! Solves for U^h
 
 	do i =1,N
-		cxarr(i) = complex(uh(i),0.0)
+		cxarr(i) = complex(uh(i),0.0d0)
 	enddo
 	
 	open(1,file='st.txt') !for debugging
 
-	call specder(0.0,real(2*pi,kind=4),N,cxarr,uhn) !spectral derivative of U^h gives us U^h_t, which is equal to u^h_n
+	call specder(0.0d0,2*pi,N,cxarr,uhn) !spectral derivative of U^h gives us U^h_t, which is equal to u^h_n
 
 	do i = 1,N
 		nhat = (/(-1)*dy(i)/sqrt(dx(i)**2+dy(i)**2),dx(i)/sqrt(dx(i)**2+dy(i)**2)/)
@@ -111,11 +111,11 @@ subroutine derpois(d1,d2,d3,infi,findif,solx,soly,sol,p,t,b,ubx,uby) !Solves poi
 		fun(i) = fux(i)*nhat(1)+fuy(i)*nhat(2)
 		fut(i) = fux(i)*that(1)+fuy(i)*that(2)
 		fut(i) = (fun(i)-upn(i))/uhn(i)
-		
+
+		write(1,*) upn(i),uhn(i)		
 		!\debugging
 		
 	enddo
-	write(1,*) L
 !	write(1,*) sum(fut)/(max(1,size(fut))), sqrt(float(size(p(:,1))))
 	
 	do i = 1,bsize !we linearly interpolate (along theta) the values of ux and uy on the boundary to the vertices of the relevant triangles
@@ -144,12 +144,12 @@ subroutine solveyouh(Gn,xin,yin,dx,dy,upx,upy,uh,N,ds) !solves linear system for
 	implicit none
 	integer::N,i,j,info
 	integer,dimension(N)::pvt
-	real(kind=8)::ds,norm
-	real(kind=8),dimension(N)::xin,yin,dx,dy,rnx,rny,upx,upy,uh,ones,rhs
-	real(kind=8),dimension(N,N)::lhs,gn
-	real(kind=8),dimension(2)::ut
-	complex(kind=8),dimension(N)::pot,potn,sigma,mu
-	complex(kind=8),dimension(N,2)::grad	
+	real *8::ds,norm
+	real *8,dimension(N)::xin,yin,dx,dy,rnx,rny,upx,upy,uh,ones,rhs
+	real *8,dimension(N,N)::lhs,gn
+	real *8,dimension(2)::ut
+	complex *16,dimension(N)::pot,potn,sigma,mu
+	complex *16,dimension(N,2)::grad	
 
 	do i = 1,N !In this loop, we build all the arrays necessary for l2dacquadwrapl to run
 		ones(i) = 1.0 
@@ -187,12 +187,12 @@ endsubroutine solveyouh
 subroutine getgnmat(Gn,xin,yin,dx,dy,ddx,ddy,N) !xin,yin should come from the arcparam subroutine, N is their length
         implicit none
         integer::N,i,j
-        real(kind=8), dimension(N,2)::xp
-        real(kind=8), dimension(N)::xin,yin,dx,dy,ddx,ddy
-        real(kind=8), dimension(N,N)::Gn
-        real(kind=8), dimension(2)::gxgy,x
-        real(kind=8)::pi
-        pi = 4*atan(1.0)
+        real *8, dimension(N,2)::xp
+        real *8, dimension(N)::xin,yin,dx,dy,ddx,ddy
+        real *8, dimension(N,N)::Gn
+        real *8, dimension(2)::gxgy,x
+        real *8::pi
+        pi = 4*atan(1.0d0)
 
         do i=1,N
                 xp(i,1) = xin(i)
@@ -214,18 +214,18 @@ end subroutine getgnmat
 
 function gx(x,xp)
         implicit none
-        real(kind=8),dimension(2)::x,xp
-        real(kind=8)::gx,pi
-        pi = 4*atan(1.0)
+        real *8,dimension(2)::x,xp
+        real *8::gx,pi
+        pi = 4*atan(1.0d0)
         
         gx =(1/(2*pi))*(xp(1)-x(1))*((x(1)-xp(1))**2+(x(2)-xp(2))**2)**(-1)
 end function gx
 
 function gy(x,xp)
         implicit none
-        real(kind=8),dimension(2)::x,xp
-        real(kind=8)::gy,pi
-        pi = 4*atan(1.0)
+        real *8,dimension(2)::x,xp
+        real *8::gy,pi
+        pi = 4*atan(1.0d0)
         
         gy=(1/(2*pi))*(xp(2)-x(2))*((x(1)-xp(1))**2+(x(2)-xp(2))**2)**(-1)
 end function gy
@@ -235,21 +235,21 @@ subroutine gradyoupee(upx,upy,d1,d2,d3,ds,nb,m,x,infi,findif) !Computes u^p on t
         use mesh
         implicit none
         integer::n,m,i,nb
-        real(kind=8), dimension(:,:), allocatable::srcloc,targloc,targnorm
-        real(kind=8), dimension(:), allocatable::srcval,psol,x,y,tarc,r,upx,upy
-        complex(kind=8), dimension(:), allocatable::pot
-        real(kind=8):: d1,d2,d3,pi,ds,l,infi,findif
-        real(kind=8),dimension(7)::args
-        real(kind=8),dimension(2)::der
+        real *8, dimension(:,:), allocatable::srcloc,targloc,targnorm
+        real *8, dimension(:), allocatable::srcval,psol,x,y,tarc,r,upx,upy
+        complex *16, dimension(:), allocatable::pot
+        real *8:: d1,d2,d3,pi,ds,l,infi,findif
+        real *8,dimension(7)::args
+        real *8,dimension(2)::der
 
-        pi = 4*atan(1.0)
+        pi = 4*atan(1.0d0)
 	call poissolve(d1,d2,d3,srcloc,x,srcval)
 	n = size(srcval)
         m = nb
         call arcparam(real(0.0,kind=8),2*pi,tarc,ds,m,l,d1,d2,d3,infi,findif)
         allocate(targloc(2,m),targnorm(2,m),pot(m),r(m),upx(m),upy(m))
 
-        args = (/d1,d2,d3,real(0.7,kind=8),real(1e-10,kind =8),real(1.0,kind=8),real(0.0,kind=8)/)
+        args = (/d1,d2,d3,0.7d0,infi,1.0d0,0.0d0/)
         do i = 1,m
                 r(i) = findr_fast(tarc(i),args)
                 targloc(1,i) = 1 + r(i)*cos(tarc(i))
@@ -276,9 +276,9 @@ SUBROUTINE specder(xmin,xmax,N,input,deriv)  !Takes spectral derivatives of orde
         integer :: N,i
         integer ::M
         complex(C_DOUBLE_COMPLEX), dimension(N) :: input,output,input2,output2
-        real(kind=8),dimension(N)::x,k,der,deriv
-        real,parameter::pi =3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862
-        real::xmin,xmax,dx
+        real *8,dimension(N)::x,k,der,deriv
+        real *8,parameter::pi =3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862
+        real *8::xmin,xmax,dx
 
 	M = N
 
@@ -318,15 +318,21 @@ END SUBROUTINE specder
 subroutine arcparam(a,b,tarc,darc,N,L,d1,d2,d3,infi,findif) !Provides N evenly spaced points along a curve parametrised by r,theta between theta = a and theta = b
 	implicit none
 	integer::N,i,j
-	real(kind=8) a,b,darc,L,tinit,tfguess,tfupdate,currerr,ds,d1,d2,d3,infi,findif
-	real(kind=8),dimension(2)::der
-	real(kind=8),dimension(:),allocatable::t,w,tarc
+	real *8 a,b,darc,L,tinit,tfguess,tfupdate,currerr,ds,d1,d2,d3,infi,findif
+	real *8,dimension(2)::der
+	real *8,dimension(:),allocatable::t,w,tarc
 	call lgmap(t,w,a,b,1)
+
+	if(infi<1e-8)then
+		infi = 1e-8
+	endif
+	
 	L = 0
-	do i = 1,size(t)
+	do i = 0,size(t)
 		der = dxdy(t(i),d1,d2,d3,findif)
 		L = L + sqrt(der(1)**2+der(2)**2)*w(i)
 	end do
+	write(*,*) l
 	darc = L/N
 	allocate(tarc(N))
 	do i = 1,N
@@ -362,8 +368,8 @@ subroutine lgmap(x,w,a,b,mode) !does 16-point or 1000-point gauss-legendre quadr
 	implicit none
 	integer::NR,i,J,ios,mode
         INTEGER, PARAMETER :: maxrecs = 1000000
-	real(kind=8),dimension(:),allocatable::x,w,xt,wt
-	real(kind=8)::a,b
+	real *8,dimension(:),allocatable::x,w,xt,wt
+	real *8::a,b
 	CHARACTER(LEN=4) :: filename
         CHARACTER(LEN=1) :: junk
 	if (mode == 0) then
@@ -411,39 +417,79 @@ end subroutine lgmap
 
 function ddxddy(theta,d1,d2,d3,infi)
 	implicit none
-	real(kind=8)::infi
-        real(kind=8) ::theta,d1,d2,d3,tp,tm
-        real(kind=8), dimension(2)::ddxddy,vec
-        real(kind=8),dimension(7)::args
-        args = (/d1,d2,d3,real(0.7,kind=8),real(infi,kind=8),real(1.0,kind=8),real(0.0,kind=8)/)
+	real *8::infi
+        real *8 ::theta,d1,d2,d3,tp,tm,dx,dy
+        real *8, dimension(2)::ddxddy,vec
+        real *8,dimension(7)::args
+        args = (/d1,d2,d3,0.7d0,infi,1.0d0,0.0d0/)
 
-	tp = theta + infi
-	tm = theta - infi
+	if(infi<1e-8)then
+		infi=1e-8
+	endif
 
-	ddxddy(1) = (findr_fast(tp,args)*cos(tp)-2*findr_fast(theta,args)*cos(theta)+findr_fast(tm,args)*cos(tm))/(infi**2)
-        ddxddy(2) = (findr_fast(tp,args)*sin(tp)+findr_fast(tm,args)*sin(tm)-2*findr_fast(theta,args)*sin(theta))/(infi**2)
+	!tp = theta + infi
+	!tm = theta - infi
+	
+	dx = 0
+	dx = (-1.0d0/12.0d0)*findr_fast(theta+2*infi,args)*cos(theta+2*infi) 
+	dx = dx + (-1.0d0/12.0d0)*findr_fast(theta-2*infi,args)*cos(theta-2*infi)
+	dx = dx + (4.0d0/3.0d0)*findr_fast(theta+infi,args)*cos(theta+infi)
+	dx = dx + (4.0d0/3.0d0)*findr_fast(theta-infi,args)*cos(theta-infi)
+	dx = dx + (-5.0d0/2.0d0)*findr_fast(theta,args)*cos(theta)
+	dx = dx/infi*infi
+
+	dy = 0
+	dy = (-1.0d0/12.0d0)*findr_fast(theta+2*infi,args)*sin(theta+2*infi) 
+	dy = dy + (-1.0d0/12.0d0)*findr_fast(theta-2*infi,args)*sin(theta-2*infi)
+	dy = dy + (4.0d0/3.0d0)*findr_fast(theta+infi,args)*sin(theta+infi)
+	dy = dy + (4.0d0/3.0d0)*findr_fast(theta-infi,args)*sin(theta-infi)
+	dx = dx + (-5.0d0/2.0d0)*findr_fast(theta,args)*sin(theta)
+	dy = dy/infi*infi
+	
+!	ddxddy(1) = (findr_fast(tp,args)*cos(tp)-2*findr_fast(theta,args)*cos(theta)+findr_fast(tm,args)*cos(tm))/(infi**2)
+ !       ddxddy(2) = (findr_fast(tp,args)*sin(tp)+findr_fast(tm,args)*sin(tm)-2*findr_fast(theta,args)*sin(theta))/(infi**2)
+
+	ddxddy(1)=dx
+	ddxddy(2)=dy
 
 end function ddxddy
 
 function dxdy(theta,d1,d2,d3,infi) !takes dx/dtheta and dy/dtheta @ theta on a tokamak defined by eps,del,kap
 	implicit none
-	real(kind=8)::infi
-	real(kind=8) ::theta,d1,d2,d3
-	real(kind=8), dimension(2)::dxdy
-	real(kind=8),dimension(7)::args
+	real *8::infi
+	real *8 ::theta,d1,d2,d3,dx,dy
+	real *8, dimension(2)::dxdy
+	real *8,dimension(7)::args
 	
-	args = (/d1,d2,d3,real(0.7,kind=8),real(infi,kind=8),real(1.0,kind=8),real(0.0,kind=8)/)
+        args = (/d1,d2,d3,0.7d0,infi,1.0d0,0.0d0/)
 	
-	dxdy(1) = (findr_fast(theta+infi,args)*cos(theta+infi)-findr_fast(theta-infi,args)*cos(theta-infi))/(2*infi)
-	dxdy(2) = (findr_fast(theta+infi,args)*sin(theta+infi)-findr_fast(theta-infi,args)*sin(theta-infi))/(2*infi)
+	dx = 0
+	dx = (-1.0d0/12.0d0)*findr_fast(theta+2*infi,args)*cos(theta+2*infi) 
+	dx = dx + (1.0d0/12.0d0)*findr_fast(theta-2*infi,args)*cos(theta-2*infi)
+	dx = dx + (2.0d0/3.0d0)*findr_fast(theta+infi,args)*cos(theta+infi)
+	dx = dx + (-2.0d0/3.0d0)*findr_fast(theta-infi,args)*cos(theta-infi)
+	dx = dx/infi
+
+	dy = 0
+	dy = (-1.0d0/12.0d0)*findr_fast(theta+2*infi,args)*sin(theta+2*infi) 
+	dy = dy + (1.0d0/12.0d0)*findr_fast(theta-2*infi,args)*sin(theta-2*infi)
+	dy = dy + (2.0d0/3.0d0)*findr_fast(theta+infi,args)*sin(theta+infi)
+	dy = dy + (-2.0d0/3.0d0)*findr_fast(theta-infi,args)*sin(theta-infi)
+	dy = dy/infi
+	
+!	dxdy(1) = (findr_fast(theta+infi,args)*cos(theta+infi)-findr_fast(theta-infi,args)*cos(theta-infi))/(2*infi)
+!	dxdy(2) = (findr_fast(theta+infi,args)*sin(theta+infi)-findr_fast(theta-infi,args)*sin(theta-infi))/(2*infi)
+
+	dxdy(1) = dx
+	dxdy(2) = dy
 
 end function dxdy
 
 function findr_fast(theta,args) !easier to call that findr itself, takes an array instead of a bunch of individual args
 	implicit none
-	real(kind=8),dimension(7)::args
-	real(kind=8)::d1,d2,d3,guess,theta,errtol,centx,centy,findif
-	real(kind=8)::findr_fast
+	real *8,dimension(7)::args
+	real *8::d1,d2,d3,guess,theta,errtol,centx,centy,findif
+	real *8::findr_fast
 	d1 = args(1)
 	d2 = args(2)
 	d3 = args(3)
@@ -459,9 +505,9 @@ end function findr_fast
 function findr(d1,d2,d3,theta,guess,errtol,centx,centy) !newton's method on a tokamak defined by eps,del,kap
 	implicit none
 	integer::i
-	real(kind=8)::d1,d2,d3,theta,findr,guess,currerr,errtol,centx,centy,rp,rm,dfr,one,pi
-	real(kind=8),dimension(2)::uvec,x,center,xp,xm
-	one = 1.0
+	real *8::d1,d2,d3,theta,findr,guess,currerr,errtol,centx,centy,rp,rm,dfr,one,pi
+	real *8,dimension(2)::uvec,x,center,xp,xm
+	one = 1.0d0
 	center(1) = centx
 	center(2) = centy
 	findr = guess
@@ -494,7 +540,7 @@ end function findr
 
 function tokam(x,y,c,d1,d2,d3) !tokamak function
 	implicit none
-	real(kind=8) :: x,y,c,d1,d2,d3,tokam
+	real *8 :: x,y,c,d1,d2,d3,tokam
 
 	tokam = c/8*x**4 + d2*x**2 + d3*(x**4-4*x**2*y**2)+d1
 end function tokam
@@ -502,9 +548,9 @@ end function tokam
 subroutine switchpars(ep,de,kap,d1,d2,d3) !switches from eps,del,kap to d1,d2,d3
 	implicit none
 	integer::info
-	real(kind=8)::ep,de,kap,d1,d2,d3,const,one
-	real(kind=8),dimension(3,3)::a,at
-	real(kind=8),dimension(3)::b,pvt
+	real *8::ep,de,kap,d1,d2,d3,const,one
+	real *8,dimension(3,3)::a,at
+	real *8,dimension(3)::b,pvt
 	one = 1.0
 	const = (-1)*(1.0/8)
 
@@ -521,7 +567,7 @@ end subroutine switchpars
 
 function interp1d(x,x1,x2,y1,y2)
 	implicit none
-	real(kind=8)::x,x1,x2,y1,y2,interp1d
+	real *8::x,x1,x2,y1,y2,interp1d
 
 	interp1d = y1*(1.0d0-(x-x1)/(x2-x1))+y2*((x-x1)/(x2-x1))
 end function interp1d
