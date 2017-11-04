1c09c2042684784ce6b9ff9d968c79cca83f67f2
Iraj Eshghi <irajeshghi@gmail.com>
2017-11-04 18:44:07 -0400

    asymmetric problem added

--- functions.f90
+++ functions.f90
@@ -60,49 +60,58 @@ contains
   gsrhsxy = 0 - 3.0d0/2.0d0*uyat/(x**3)!3./4*uxyat/(x**2) 
   endfunction gsrhsxy
 
-  function exact(x,y,c,d1,d2,d3) !known solution to GS equation with 0 boundary conditions on a tokamak with parameters d1,d2,d3
+  function exact(x,y,c,d1,d2,d3,d4) !known solution to GS equation with 0 boundary conditions on a tokamak with parameters d1,d2,d3
   implicit none
   real *8::pi
-  real *8,intent(in):: x,y,c,d1,d2,d3
+  real *8,intent(in):: x,y,c,d1,d2,d3,d4
   real *8:: exact
   pi = 4*atan(1.0)
+    
+  !THIS IS AN EXACT SOLUTION FOR PSI, NOT U
 
-  exact = (c/8.0*x**4+d1+d2*x**2+d3*(x**4-4*(x**2)*(y**2)))/sqrt(x)
+
+!  exact = (c/8.0*x**4+d1+d2*x**2+d3*(x**4-4*(x**2)*(y**2)))/sqrt(x)
+  exact = x**4*((1-c)/8+d3)+x**2*(c/2*log(x)+d2-4*y**2*d3)+d1+d4*y
   end function exact
   
-  function exactx(x,y,c,d1,d2,d3) !x derivative of solution
+  function exactx(x,y,c,d1,d2,d3,d4) !x derivative of solution
   implicit none
-  real *8::x,y,c,d1,d2,d3,exactx
+  real *8::x,y,c,d1,d2,d3,d4,exactx
 
-  exactx = ((x**2)*(24*d2+7*(8*d3+c)*(x**2)-96*d3*y**2)-8*d1)/(16*x**(3.0d0/2.0d0))
+  !exactx = ((x**2)*(24*d2+7*(8*d3+c)*(x**2)-96*d3*y**2)-8*d1)/(16*x**(3.0d0/2.0d0))
+  exactx = x**3*((1-c)/2+4*d3)+x*(c*log(x)+2*d2-8*y**2*d3+c/2)
   endfunction exactx
 
-  function exacty(x,y,c,d1,d2,d3) !y derivative of solution
+  function exacty(x,y,c,d1,d2,d3,d4) !y derivative of solution
   implicit none
-  real *8::x,y,c,d1,d2,d3,exacty
+  real *8::x,y,c,d1,d2,d3,d4,exacty
 
-  exacty =  (-8)*d3*y*x**(3.0d0/2.0d0)
+!  exacty =  (-8)*d3*y*x**(3.0d0/2.0d0)
+  exacty = d4 - 8*x**2*y*d3
   endfunction exacty
 
-  function exactxx(x,y,c,d1,d2,d3) !xx derivative of solution
+  function exactxx(x,y,c,d1,d2,d3,d4) !xx derivative of solution
   implicit none
-  real *8::x,y,c,d1,d2,d3,exactxx
+  real *8::x,y,c,d1,d2,d3,d4,exactxx
 
-  exactxx = (24*d1+(x**2)*(24*d2+(x**2)*35*(8*d3+c)-96*d3*(y**2)))/(32*(x**(5.0d0/2.0d0)))
+!  exactxx = (24*d1+(x**2)*(24*d2+(x**2)*35*(8*d3+c)-96*d3*(y**2)))/(32*(x**(5.0d0/2.0d0)))
+  exactxx = x**2*(12*d3+3*(1-c)/2) + c*log(x)+c+c/2+2*d2-8*y**2*d3
   endfunction exactxx
 
-  function exactxy(x,y,c,d1,d2,d3) !xy derivative of solution
+  function exactxy(x,y,c,d1,d2,d3,d4) !xy derivative of solution
   implicit none
-  real *8::x,y,c,d1,d2,d3,exactxy
+  real *8::x,y,c,d1,d2,d3,d4,exactxy
 
-  exactxy = (-12)*d3*sqrt(x)*y
+!  exactxy = (-12)*d3*sqrt(x)*y
+  exactxy = -16*x*y*d3
   endfunction exactxy
 
-  function exactyy(x,y,c,d1,d2,d3) !yy derivative of solution
+  function exactyy(x,y,c,d1,d2,d3,d4) !yy derivative of solution
   implicit none
-  real *8::x,y,c,d1,d2,d3,exactyy
-  exactyy = (-8)*d3*x**(3.0d0/2.0d0)
+  real *8::x,y,c,d1,d2,d3,d4,exactyy
+!  exactyy = (-8)*d3*x**(3.0d0/2.0d0)
 
+  exactyy = -8*d3*x**2
   endfunction exactyy
 
 endmodule functions
